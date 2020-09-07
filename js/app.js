52845aae27eb877de98e2ba275a1e373edee0302
hanandito <hananditos@gmail.com>
2020-09-07 17:53:10 +0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+    $('.nav-birthday-2 .fa-bars').click(function() {
+     $(".side-nav").addClass("active");
+   })
+   $('.close-div .fa-times, .side-nav li').click(function() {
+     $(".side-nav").removeClass("active");
+   })
+//    Do me a Favor
+ $('.carousel-flavor').addClass('owl-carousel owl-theme').owlCarousel({
+     loop: true,
+     responsiveClass: true,
+     navText: ["<img src='images/chevron-left.png'>","<img src='images/chevron-right.png'>"],
+     responsive:{
+       0: {
+         items: 1,
+         dots: true,
+         nav: false
+       },
+       600: {
+         items: 1,
+         dots: true,
+         nav: false
+       },
+       1000: {
+         items: 1,
+         dots: false,
+         nav: true
+       }
+     }
+   })
+   /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-120px";
+    }
+    prevScrollpos = currentScrollPos;
+    }
+ })
+ $(document).ready(function(){
+   // Add smooth scrolling to all links
+   $(".nav-birthday-2 a, .how-to-drink a, .testimonial a").on('click', function(event) {
+ 
+     // Make sure this.hash has a value before overriding default behavior
+     if (this.hash !== "") {
+       // Prevent default anchor click behavior
+       event.preventDefault();
+ 
+       // Store hash
+       var hash = this.hash;
+ 
+       // Using jQuery's animate() method to add smooth page scroll
+       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+       $('html, body').animate({
+         scrollTop: $(hash).offset().top
+       }, 1000, function(){
+ 
+         // Add hash (#) to URL when done scrolling (default click behavior)
+         window.location.hash = hash;
+       });
+     } // End if
+   });
+ });
